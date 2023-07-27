f1f103c6807ae6ff0b991b65aa7c8631cbb46766
Jean Martins <jeanmrtns4@gmail.com>
2023-07-27 16:49:05 -0300

    feat: create get transactions and summary routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,40 @@ import { z } from 'zod';
 import { randomUUID } from 'crypto';
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*');
+
+    return {
+      transactions,
+    };
+  });
+
+  app.get('/:id', async (request) => {
+    const requestParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+    const { id } = requestParamsSchema.parse(request.params);
+    const transaction = await knex('transactions')
+      .where({
+        id,
+      })
+      .first();
+
+    return {
+      transaction,
+    };
+  });
+
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', {
+        as: 'amount',
+      })
+      .first();
+
+    return { summary };
+  });
+
   app.post('/', async (request, reply) => {
     const requestBodySchema = z.object({
       title: z.string(),
@@ -20,11 +54,5 @@ export async function transactionsRoutes(app: FastifyInstance) {
     });
 
     return reply.status(201).send();
-  });
-
-  app.get('/', async (request, reply) => {
-    const transactions = await knex('transactions').select('*');
-
-    return reply.send(transactions);
   });
 }
