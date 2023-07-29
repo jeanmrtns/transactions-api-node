4269412a673e4ba71be41393ea01abb1bd54fcb0
Jean Martins <jeanmrtns4@gmail.com>
2023-07-28 21:39:57 -0300

    feat: create application logger

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import { transactionsRoutes } from './routes/transactions';
 export const app = fastify();
 
 app.register(cookies);
+app.addHook('preHandler', (request) => {
+  console.log(`[${request.method}]: ${request.url}`);
+});
 app.register(transactionsRoutes, {
   prefix: 'transactions',
 });
